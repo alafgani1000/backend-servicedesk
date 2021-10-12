757141f624c119b396697e402a989050ed206c7d
alafgani1000 <cppol08@gmail.com>
2021-10-12 07:40:07 +0700

    get data count incident and r equest

--- apps/controllers/DashboardController.js
+++ apps/controllers/DashboardController.js
@@ -3,6 +3,7 @@ const multer = require('multer');
 const moment = require('moment');
 const dbconfig = require('../configs/db.config');
 const Incidents = dbconfig.incidents;
+const Requests = dbconfig.requests;
 const IncidentAttachments = dbconfig.incidentAttachments;
 const Stages = dbconfig.stages;
 const Teams = dbconfig.teams;
@@ -10,16 +11,80 @@ const Categories = dbconfig.categories;
 const Users = dbconfig.users;
 
 exports.countIncidentEmit = async socket => {
-    const jumIncident = await Incidents.findAll({
-        attributes:[[Sequelize.fn('COUNT', Sequelize.col('id')), 'count'] ]
-    })
-    .then(result => {
+    const stageOpen = await Incidents.findAll({
+        attributes:[[Sequelize.fn('COUNT', Sequelize.col('id')), 'count'] ],
+        where:{ id: { [Op.eq]: 'Open' }}
+    }).then(result => {
         return result[0].dataValues.count
     });
 
-    // const response = new Date();
-    socket.emit('jumIncident', jumIncident);
-};
+    const stageNew = await Incidents.findAll({
+        attributes:[[Sequelize.fn('COUNT', Sequelize.col('id')), 'count']],
+        where:{ id: {[Op.eq]: 'New'}}
+    }).then(result => {
+        return result[0].dataValues.count
+    })
+
+    const stageClose = await Incidents.findAll({
+        attributes:[[Sequelize.fn('COUNT', Sequelize.col('id')), 'count']],
+        where:{ id: {[Op.eq]: 'Close'}}
+    }).then(result => {
+        return result[0].dataValues.count
+    })
+
+    const stageResolve = await Incidents.findAll({
+        attributes:[[Sequelize.fn('COUNT', Sequelize.col('id')), 'count']],
+        where:{ id: {[Op.eq]: 'Resolve'}}
+    }).then(result => {
+        return result[0].dataValues.count
+    })
+
+    let data = {
+        "new":stageNew,
+        "open":stageOpen,
+        "resolve":stageResolve,
+        "close":stageClose
+    }
+    socket.emit('countIncident', data);
+}
+
+exports.countRequestEmit =  async socket => {
+    const stageNew = await Requests.findAll({
+        attributes:[[Sequelize.fn('COUNT',Sequelize.col('id')), 'count']],
+        where:{ id:{ [Op.eq]: 'New' } }
+    }).then(result => {
+        return result[0].dataValues.count
+    })
+
+    const stageOpen = await Requests.findAll({
+        attributes:[[Sequelize.fn('COUNT',Sequelize.col('id')), 'count']],
+        where:{ id:{ [Op.eq]: 'Open' } }
+    }).then(result => {
+        return result[0].dataValues.count
+    })
+
+    const stageResolve = await Requests.findAll({
+        attributes:[[Sequelize.fn('COUNT',Sequelize.col('id')), 'count']],
+        where:{ id:{ [Op.eq]: 'Resolve' } }
+    }).then(result => {
+        return result[0].dataValues.count
+    })
+
+    const stageClose = await Requests.findAll({
+        attributes:[[Sequelize.fn('COUNT',Sequelize.col('id')), 'count']],
+        where:{ id:{ [Op.eq]: 'Close' } }
+    }).then(result => {
+        return result[0].dataValues.count
+    })
+
+    let data = {
+        "new":stageNew,
+        "open":stageOpen,
+        "resolve":stageResolve,
+        "close":stageClose
+    }
+    socket.emit('countRequest', data);
+}
 
 exports.countIncidentOpen = async socket => {
     const jumIncidentOpen = await Incidents.findAll({
@@ -32,3 +97,4 @@ exports.countIncidentOpen = async socket => {
 
     socket.emit('jumIncidentOpen', jumIncidentOpen);
 }
+
