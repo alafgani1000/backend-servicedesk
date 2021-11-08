39270c099036a9074d1addead5948158c83c3e6e
alafgani1000 <cppol08@gmail.com>
2021-11-08 15:15:22 +0700

    create model load Goods

---
+++ apps/models/LoanGoods.js
@@ -0,0 +1,40 @@
+const { DataTypes } = require('sequelize/types');
+const sequelize = require('../configs/connection');
+
+const Menu = sequelize.define('loan_goods', {
+    id: {
+        type: DataTypes.INTEGER(11),
+        primaryKey: true,
+        allowNull: false
+    },
+    nopegawai: {
+        type: DataTypes.STRING(10),
+        allowNull: false
+    },
+    kode_barang: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    nama_barang: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    tanggal_pinjam: {
+        type: DataTypes.DATEONLY,
+        allowNull: false
+    },
+    waktu_pinjam: {
+        type: DataTypes.TIME,
+        allowNull: false
+    },
+    tanggal_kembali: {
+        type: DataTypes.DATEONLY,
+        allowNull: true
+    },
+    waktu_kembali: {
+        type: DataTypes.TIME,
+        allowNull: true
+    }
+});
+
+module.exports = Menu;
